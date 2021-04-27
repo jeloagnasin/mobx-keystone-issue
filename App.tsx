31da7350af155ec7d89496bb245e33ebdd6bb329
Jelo <jelo@icannhas.com>
2021-04-28 06:53:35 +0800

    Create simplest reproduction of the issue

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
+import 'react-native-get-random-values';
 import { StatusBar } from 'expo-status-bar';
+import { model, Model } from 'mobx-keystone';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+@model('mobx-keystone-issue/Store')
+class Store extends Model({}) {}
+
 export default function App() {
+  const store = new Store({});
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
